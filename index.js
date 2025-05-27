38522b84a68e0ad13b1a4fb26726e5fddbf67ae3
Constanza <coty.serrano001@gmail.com>
2025-05-27 17:38:57 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//VAR ESTA COMPLETAMENTE PROHIBIDO
+
+//let= es modificable;
+//const= no es modificable;
+//let nombre=constanza; //lo declara primero y despues inicializa alojando un valor 
+//let constanza; //directamente lo declara
+
+
+const titutlo= document.getElementById("title");
+titutlo.textContent="titutlo nuevo"
