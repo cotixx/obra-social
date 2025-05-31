f2cc7d771d4161ff65a86e7051f22eae0be633fc
Constanza <coty.serrano001@gmail.com>
2025-05-31 10:31:03 -0300

    funcion iniciar sesion

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 
 //REGISTRO DE USUARIO NUEVO
 
-const usuario= [];
+const usuario= {
+    credencial:"",
+    nombre:"",
+    contrasena:"" ,
+    correo:"",
+    telefono:""
+};
 const crearCuenta = document.getElementById("cuentaNueva");
 
 crearCuenta.onclick= function crearUsuario(){
     let credencial=prompt("Ingresar credencial como usuario: ");
-    if (usuario[0] === credencial){
+    if (usuario.credencial === credencial){
         alert("Ya existe usuario con esta credencial") 
         return;
     }else if(usuario!="" || usuario!=null) {
-        usuario.splice(0,usuario.length);
+        delete usuario;
     }
     while(credencial === ""){
         credencial=prompt("Ingresar credencial como usuario: ");
+    }
+    let nombre=prompt("Ingresar nombre y apellido: ");
+    while(nombre=="" || nombre===null){
+        nombre=prompt("Ingresar nombre y apellido: ");
     }
     let contrasena=prompt("Ingresar contraseña: ");
     while(contrasena=="" || contrasena===null){
@@ -22,20 +32,40 @@ crearCuenta.onclick= function crearUsuario(){
     let correo=prompt("Ingresar correo: ");
     while(correo=="" || correo===null || correo===usuario[2]){
         correo=prompt("Ingresar correo válido: ");
-       
     }
     let telefono=prompt("Ingresar telefono: ");
     while(telefono=="" || telefono===null){
         telefono=prompt("Ingresar telefono: ");
     }
     if(credencial && contrasena != ""){
-        usuario.push(credencial,contrasena,correo,telefono);
+        usuario.credencial=credencial;
+        usuario.nombre=nombre;
+        usuario.contrasena=contrasena;
+        usuario.correo=correo;
+        usuario.telefono=telefono;
     }else{
         return alert("Hubo un error vuelva a intentarlo");
     }
     console.log(usuario);
-    alert("Sus datos fueron guardados correctamente. \n Usuario: "+usuario[0] +"\n Contraseña: "+usuario[1]+"\n Correo: "+usuario[2]+"\n Teléfono: "+usuario[3]);
+    alert("Sus datos fueron guardados correctamente. \n Usuario: "+usuario.credencial +"\n Nombre: "+usuario.nombre+"\n Contraseña: "+usuario.contrasena+"\n Correo: "+usuario.correo+"\n Teléfono: "+usuario.telefono);
 };  
+console.log(usuario);
+
+
+
 
 //INGRESO USUARIO EXISTENTE
 
+const iniciarCuenta = document.getElementById("ingresarCuenta");
+
+iniciarCuenta.onclick = function iniciarUsuario(){
+    let credencial=prompt("Ingrese Usuario: ");
+    let contrasena=prompt("Ingresar contraseña: ");
+    if(credencial !=usuario.credencial || contrasena!=usuario.contrasena){
+        return alert("El usuario o contraseña son inválidos, vuelva a intentar.")
+    }else if(credencial===usuario.credencial && contrasena===usuario.contrasena){
+        alert("Bienvenidx, "+ usuario.nombre);
+    }else{
+        return alert ("Ocurrió un error, vuelva a intentar en unos segundos.");
+    }
+}
